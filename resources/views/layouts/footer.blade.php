d0c3ada9e7615c6d7d554fed145525b9a3d5522c
ddanya6327 <69433959+ddanya6327@users.noreply.github.com>
2021-04-06 18:37:28 +0900

    add footer blade

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <a href="/" class="footer__button--selected">Today</a>
+    <span class="vertical-line"></span>
+    <a href="/">Daily</a>
+    <span class="vertical-line"></span>
+    <a href="/">Monthly</a>
+</footer>
