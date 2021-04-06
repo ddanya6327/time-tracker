b1742181e4a674c01fff873a7493e2b1f98ac7a3
ddanya6327 <69433959+ddanya6327@users.noreply.github.com>
2021-04-06 18:07:36 +0900

    mark up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,41 @@
 <div class="container">
     <div class="tracking-data">
         <div class="user-status">
-            user status
+            <div class="user-status__name">
+                Hello, hyoek!
+            </div>
+            <div class="user-status__level">
+                <div class="level__text">
+                    Lv. 17
+                </div>
+                <div class="level__bar">
+                    80%
+                </div>
+            </div>
         </div>
+
         <div class="tracking-list">
-            time list
+            <div class="list__item">
+                <div class="item__title">
+                    Reading - 1 hour
+                </div>
+                <div class="item__description">
+                    I read the little prince up to page 55.
+                </div>
+            </div>
+            <div class="list__item">
+                <div class="item__title">
+                    Reading - 2 hour
+                </div>
+                <div class="item__description">
+                    I read the little prince up to page 33.
+                </div>
+            </div>
         </div>
+
         <div class="tracking-buttons">
-            buttons
+            <button>Time recoding</button>
+            <button>Edit tag</button>
         </div>
     </div>
 </div>
